8e03ec7760f74e301bc96dcba8a8c81bf1268c9d
Collins Ikechukwu <collinschristroa@gmail.com>
2024-12-04 18:32:20 +0100

    refactor: replace Header component with Hero component in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
   return (
     <div className="">
       <Navbar />
-      <Header />
-      {/* <Hero /> */}
+      {/* <Header /> */}
+      <Hero />
       {/* <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
